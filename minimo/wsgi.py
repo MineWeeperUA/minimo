7b829276de48f47b9511ad3c6305987b2ead645b
Oleh <oleh.kozachenko@gmail.com>
2020-09-06 10:35:11 +0300

    Added files and changes required for deployment to heroku

--- minimo/wsgi.py
+++ minimo/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for minimo project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minimo.settings')
-
 application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
