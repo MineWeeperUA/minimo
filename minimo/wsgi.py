f1043ca0c016294872857c6dc7c906e77df37f7a
Oleh <oleh.kozachenko@gmail.com>
2020-09-06 10:42:58 +0300

    Added files and changes required for deployment to heroku

--- minimo/wsgi.py
+++ minimo/wsgi.py
@@ -1,9 +1,6 @@
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
